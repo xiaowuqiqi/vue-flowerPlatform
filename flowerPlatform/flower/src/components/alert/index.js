886920dfd2a458332678ce211437523a40dce361
wallacety <2805080748@qq.com>
2019-02-28 15:46:39 +0800

    remove flowerPlatform

--- flowerPlatform/flower/src/components/alert/index.js
+++
@@ -1,7 +0,0 @@
-import AlertVue from './Alert.vue'
-const Alert = {
-  install: function(Vue){
-    Vue.component('Alert',AlertVue)
-  }
-}
-export default Alert
--- flowerPlatform/flower/src/components/modal/index.js
+++
@@ -1,7 +0,0 @@
-import ModalVue from './Modal.vue'
-const Modal = {
-  install: function(Vue){
-    Vue.component('Modal',ModalVue)
-  }
-}
-export default Modal
